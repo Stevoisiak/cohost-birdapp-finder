3cf350f7dd8b3fc4f0e9bc8ec8d4e3d3ffd4c661
Stevoisiak <stevoisiak@gmail.com>
2022-11-09 17:54:27 -0500

    Rename variables to remove twitter_

--- twitter-evacuee-finder.py
+++ twitter-evacuee-finder.py
@@ -6,17 +6,17 @@ def main():
     # Get list of twitter follows for a user then check if accounts exist on Cohost 
 
     # https://developer.twitter.com/en
-    twitter_bearer_token = input("Enter twitter bearer token. (Obtained from https://developer.twitter.com/en)\nBearer Token: ")
-    twitter_user_id = input("Enter your twitter account's user ID. (Obtained from https://tweeterid.com/)\nUser ID: ")
-    twitter_url = f"https://api.twitter.com/2/users/{twitter_user_id}/following"
-    twitter_params = {"max_results": 1000}
-    twitter_headers = {"Authorization": f"Bearer {twitter_bearer_token}"}
+    bearer_token = input("Enter twitter bearer token. (Obtained from https://developer.twitter.com/en)\nBearer Token: ")
+    user_id = input("Enter your twitter account's user ID. (Obtained from https://tweeterid.com/)\nUser ID: ")
+    url = f"https://api.twitter.com/2/users/{user_id}/following"
+    params = {"max_results": 1000}
+    headers = {"Authorization": f"Bearer {bearer_token}"}
 
     # Get follows list from Twitter
     # TODO: Limited to 1000 followers. Requires pagation support.
     #       https://developer.twitter.com/en/docs/twitter-api/pagination
-    input(f"Press Enter to get Twitter follow list for {twitter_user_id}...")
-    r = requests.get(twitter_url, params=twitter_params, headers=twitter_headers, timeout=10)
+    input(f"Press Enter to get Twitter follow list for {user_id}...")
+    r = requests.get(url, params=params, headers=headers, timeout=10)
     if r.status_code != 200:
         raise Exception(
             "Request returned an error: {} {}".format(
@@ -34,10 +34,10 @@ def main():
 
         # Check if cohost account exists
         # print(f"Checking cohost for user {username}.")
-        request_url = f'https://cohost.org/{username}'
-        r = requests.head(request_url, timeout=10)
+        url = f'https://cohost.org/{username}'
+        r = requests.head(url, timeout=10)
         if r.status_code == 200:
-            print(u["name"] + " --> " + request_url)
+            print(u["name"] + " --> " + url)
         elif r.status_code == 404:
             pass
             # print("Cohost account does not exist (404)")
