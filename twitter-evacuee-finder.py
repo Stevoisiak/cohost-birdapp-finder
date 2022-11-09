e2450fafaaa76bd3845b325281b880a6583d1f6e
Stevoisiak <stevoisiak@gmail.com>
2022-11-09 13:15:08 -0500

    User input for bearer token and user id

--- twitter-evacuee-finder.py
+++ twitter-evacuee-finder.py
@@ -1,16 +1,13 @@
 import requests
-import json
-import keyring
 
 # Retrieving twitter follows based on https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Follows-Lookup/following_lookup.py
-
-# Get bearer token
-twitter_bearer_token = keyring.get_password("twitter_evacuee_finder", "bearer_token")
 
 def main():
     # Get list of twitter follows for a user then check if accounts exist on Cohost 
 
-    twitter_user_id = 92179256  # Stevoisiak
+    # https://developer.twitter.com/en
+    twitter_bearer_token = input("Enter twitter bearer token. (Obtained from https://developer.twitter.com/en)\nBearer Token: ")
+    twitter_user_id = input("Enter your twitter account's user ID. (Obtained from https://tweeterid.com/)\nUser ID: ")
     twitter_url = f"https://api.twitter.com/2/users/{twitter_user_id}/following"
     twitter_params = {"max_results": 1000}
     twitter_headers = {"Authorization": f"Bearer {twitter_bearer_token}"}
