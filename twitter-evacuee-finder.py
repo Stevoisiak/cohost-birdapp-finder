a9f9922ce2f8c7fddfb5bb5dc4b076c17a5f807a
Stevoisiak <stevoisiak@gmail.com>
2022-11-10 11:52:39 -0500

    Write results to csv file

--- twitter-evacuee-finder.py
+++ twitter-evacuee-finder.py
@@ -1,11 +1,11 @@
+import csv
 import requests
 
+# Check follows for a Twitter account and see if accounts with the same username exist on Cohost
 # Retrieving twitter follows based on https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Follows-Lookup/following_lookup.py
 
 def main():
-    # Get list of twitter follows for a user then check if accounts exist on Cohost 
 
-    # https://developer.twitter.com/en
     bearer_token = input("Enter twitter bearer token. (Obtained from https://developer.twitter.com/en)\nBearer Token: ")
     user_id = input("Enter your twitter account's user ID. (Obtained from https://tweeterid.com/)\nUser ID: ")
     url = f"https://api.twitter.com/2/users/{user_id}/following"
@@ -15,7 +15,7 @@ def main():
     # Get follows list from Twitter
     # TODO: Limited to 1000 followers. Requires pagation support.
     #       https://developer.twitter.com/en/docs/twitter-api/pagination
-    input(f"Press Enter to get Twitter follow list for {user_id}...")
+    input("Press Enter to get Twitter follow list...")
     r = requests.get(url, params=params, headers=headers, timeout=10)
     if r.status_code != 200:
         raise Exception(
@@ -27,8 +27,8 @@ def main():
     print(f"Retrieved {json_response['meta']['result_count']} follows")
     # print(json.dumps(json_response, indent=4, sort_keys=True))
 
-    # Iterate through twitter users
-    input(f"Press Enter to check for cohost accounts...")
+    # Check each twitter username for a cohost account
+    users = []
     for u in json_response["data"]:
         username = u['username']
 
@@ -37,16 +37,32 @@ def main():
         url = f'https://cohost.org/{username}'
         r = requests.head(url, timeout=10)
         if r.status_code == 200:
+            user = {
+                'twitter_id': u['id'],
+                'twitter_display_name': u['name'],
+                'twitter_username': u['username'],
+                'cohost_url': url
+            }
+            users.append(user)
             print(u["name"] + " --> " + url)
         elif r.status_code == 404:
+            # Cohost account does not exist
             pass
-            # print("Cohost account does not exist (404)")
         else:
             raise Exception(
                 "Cohost request returned an error: {} {}".format(
                     r.status_code, r.text
                 )
             )
+
+    # TODO: Error handling if writing to file fails
+    print(f"Found {len(users)} Cohost accounts. Writing to file...")
+    with open('output.csv', 'w', newline='', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"')
+        csv_writer.writerow(['Twitter ID', 'Twitter Display Name', 'Username', 'Cohost URL'])
+        for u in users:
+            csv_writer.writerow([u['twitter_id'], u['twitter_display_name'], u['twitter_username'], u['cohost_url']])
+    print("Finished saving to output.csv")
 
 if __name__ == "__main__":
     main()
